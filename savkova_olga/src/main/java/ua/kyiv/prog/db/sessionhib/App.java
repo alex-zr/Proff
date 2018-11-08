57137515e43558ff24e009cb7833eb64fd9fa74a
Olga Savkova <olga.savkova@2me.link>
2018-11-08 23:34:46 +0200

    refactoring

--- savkova_olga/src/main/java/ua/kyiv/prog/db/sessionhib/App.java
+++ savkova_olga/src/main/java/ua/kyiv/prog/db/sessionhib/App.java
@@ -28,18 +28,21 @@ public class App {
             showAllClients();
             System.out.println();
 
-            long id = 3L;
+            long id = 3;
             changeClientName(id);
             SimpleClient simpleClient = em.find(SimpleClient.class, id);
             System.out.println(simpleClient);
             System.out.println();
 
-            id = 4L;
+            id = 4;
             removeClient(id);
             System.out.println();
 
-            addClientToGroup(1L, 2L);
-            addClientToGroup(2L, 2L);
+            SimpleClient client1 = getClient(1L);
+            SimpleClient client2 = getClient(2L);
+            Group group = getGroup(2L);
+            addClientToGroup(client1, group);
+            addClientToGroup(client2, group);
 
         } catch (HibernateException ex) {
             ex.printStackTrace();
@@ -55,25 +58,15 @@ public class App {
     }
 
     private static void changeClientName(long id) {
-        SimpleClient client;
-        try {
-            Query query = em.createQuery("SELECT c FROM SimpleClient c where c.id=:id", SimpleClient.class);
-            query.setParameter("id", id);
-            client = (SimpleClient) query.getSingleResult();
-        } catch (NoResultException e) {
-            System.out.println("Client not found");
-            return;
-        } catch (NonUniqueResultException e) {
-            System.out.println("Non unique result");
-            return;
-        }
-
-        em.getTransaction().begin();
-        try {
-            client.setName("Vadim");
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            em.getTransaction().rollback();
+        SimpleClient client = getClient(id);
+        if (client != null) {
+            em.getTransaction().begin();
+            try {
+                client.setName("Vadim");
+                em.getTransaction().commit();
+            } catch (Exception e) {
+                em.getTransaction().rollback();
+            }
         }
     }
 
@@ -92,33 +85,7 @@ public class App {
         }
     }
 
-    private static void addClientToGroup(long clientId, long groupId) {
-        SimpleClient client;
-        try {
-            Query query = em.createQuery("SELECT c FROM SimpleClient c where c.id=:id", SimpleClient.class);
-            query.setParameter("id", clientId);
-            client = (SimpleClient) query.getSingleResult();
-        } catch (NoResultException e) {
-            System.out.println("Client with id=" + clientId + "not found");
-            return;
-        } catch (NonUniqueResultException e) {
-            System.out.println("Non unique result");
-            return;
-        }
-
-        Group group;
-        try {
-            Query query = em.createQuery("SELECT c FROM Group c where c.id=:id", Group.class);
-            query.setParameter("id", groupId);
-            group = (Group) query.getSingleResult();
-        } catch (NoResultException e) {
-            System.out.println("Group with id=" + groupId + " not found");
-            return;
-        } catch (NonUniqueResultException e) {
-            System.out.println("Non unique result");
-            return;
-        }
-
+    private static void addClientToGroup(SimpleClient client, Group group) {
         em.getTransaction().begin();
         try {
             client.setGroup(group);
@@ -126,7 +93,34 @@ public class App {
         } catch (Exception e) {
             em.getTransaction().rollback();
         }
+    }
 
+    private static SimpleClient getClient(long id) {
+        try {
+            Query query = em.createQuery("SELECT c FROM SimpleClient c where c.id=:id", SimpleClient.class);
+            query.setParameter("id", id);
+            return (SimpleClient) query.getSingleResult();
+        } catch (NoResultException e) {
+            System.out.println("Client with id=" + id + "not found");
+            return null;
+        } catch (NonUniqueResultException e) {
+            System.out.println("Non unique result");
+            return null;
+        }
+    }
+
+    private static Group getGroup(long id) {
+        try {
+            Query query = em.createQuery("SELECT c FROM Group c where c.id=:id", Group.class);
+            query.setParameter("id", id);
+            return (Group) query.getSingleResult();
+        } catch (NoResultException e) {
+            System.out.println("Group with id=" + id + " not found");
+            return null;
+        } catch (NonUniqueResultException e) {
+            System.out.println("Non unique result");
+            return null;
+        }
     }
 
 }
