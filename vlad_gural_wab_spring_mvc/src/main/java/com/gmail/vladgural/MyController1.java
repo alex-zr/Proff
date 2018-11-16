01f987f6080f6bfc58761cee45507f0ce78f0594
Vladyslav Gural <vladgural@gmail.com>
2018-11-17 00:53:17 +0200

    JPA HomeWork 2 (Bank)

---
+++ vlad_gural_wab_spring_mvc/src/main/java/com/gmail/vladgural/MyController1.java
@@ -0,0 +1,45 @@
+package com.gmail.vladgural;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
+public class MyController1 {
+
+    static Map<String, String> map = new HashMap<String, String>();
+
+    static {
+        map.put("user1", "password1");
+        map.put("user2", "password2");
+    }
+
+    @RequestMapping("/old")
+    public String onIndex() {
+        return "index_old";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String onLogin(Model model,
+                          @RequestParam String login,
+                          @RequestParam String password,
+                          HttpServletRequest request) {
+        String pass = map.get(login);
+
+        model.addAttribute("login", login);
+        if (password.equals(pass)) {
+            model.addAttribute("message", "Success");
+        } else {
+            model.addAttribute("message", "Failure");
+        }
+
+        return "result_old";
+    }
+}
